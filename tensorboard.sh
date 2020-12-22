8ad68175f55250465f094527a56bbbdf32b83372
jiahuei <tanjiahuei@gmail.com>
2020-12-22 12:26:57 +0800

    Update tensorboard.sh LOG_DIR

--- tensorboard.sh
+++ tensorboard.sh
@@ -2,7 +2,7 @@
 
 export CUDA_VISIBLE_DEVICES=""
 
-LOG_DIR="/home/jiahuei/Documents/1_TF_files/relation_trans/mscoco_v1_tests"
+LOG_DIR="/home/jiahuei/Documents/1_TF_files/relation_trans/mscoco_v1"
 
 tensorboard --logdir=${LOG_DIR} --host 0.0.0.0
 
