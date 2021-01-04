55ecb2e9c9616f5f52bc12674336a34e214254ae
jiahuei <tanjiahuei@gmail.com>
2021-01-04 18:50:13 +0800

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -10,6 +10,51 @@ export STANZA_CACHE_DIR="${DATASET_DIR}/stanza_resources"
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export CUDA_VISIBLE_DEVICES="0"
 
+python /master/src/caption_vae/eval_model.py \
+    --log_dir ${LOG_DIR} \
+    --beam_size_test 2 \
+    --eval_dir_suffix TESTING \
+    --model_file model_best.pth \
+    --id RTrans__baseline
+
+python /master/src/caption_vae/eval_model.py \
+    --log_dir ${LOG_DIR} \
+    --beam_size_test 2 \
+    --eval_dir_suffix TESTING \
+    --id RTrans__supermask__0.90__SCST_sample_baseline_s15_e10_C1B0001
+
+python /master/src/caption_vae/eval_model.py \
+    --log_dir ${LOG_DIR} \
+    --beam_size_test 2 \
+    --eval_dir_suffix TESTING \
+    --id RTrans__supermask__0.90__wg_120.0
+
+python /master/src/caption_vae/eval_model.py \
+    --log_dir ${LOG_DIR} \
+    --beam_size_test 2 \
+    --eval_dir_suffix TESTING \
+    --model_file model_best.pth \
+    --id UpDownLSTM__baseline
+
+python /master/src/caption_vae/eval_model.py \
+    --log_dir ${LOG_DIR} \
+    --beam_size_test 2 \
+    --eval_dir_suffix TESTING \
+    --model_file model_best.pth \
+    --id UpDownLSTM__baseline__SCST_sample_baseline_s60_e10_C1B0001
+
+python /master/src/caption_vae/eval_model.py \
+    --log_dir ${LOG_DIR} \
+    --beam_size_test 2 \
+    --eval_dir_suffix TESTING \
+    --id UpDownLSTM__supermask__0.991__wg_120.0
+
+python /master/src/caption_vae/eval_model.py \
+    --log_dir ${LOG_DIR} \
+    --beam_size_test 2 \
+    --eval_dir_suffix TESTING \
+    --id RTrans__supermask__0.991__wg_120.0
+
 
 ######################
 # Up-Down LSTM
@@ -20,6 +65,11 @@ MODEL_TYPE="up_down_lstm"
 MODEL_ID="UpDownLSTM"
 SCHEDULER="cosine"
 
+# Pruning
+PRUNE_TYPE="supermask"
+PRUNE_SPARSITY_TARGET=0.9875
+PRUNE_WEIGHT=120
+
 python /master/src/caption_vae/train_transformer.py \
     --caption_model ${MODEL_TYPE} \
     --dataset_dir ${DATASET_DIR} \
@@ -27,39 +77,35 @@ python /master/src/caption_vae/train_transformer.py \
     --lr_scheduler ${SCHEDULER} \
     --learning_rate 0.01 \
     --optim_epsilon 0.01 \
-    --id ZZZZZ_${MODEL_ID}__baseline \
+    --batch_size 3 \
+    --batch_size_eval 3 \
+    --id TESTING_${MODEL_ID}__baseline \
     --save_checkpoint_every 10 \
     --cache_min_free_ram ${CACHE_FREE_RAM}
 
 # Pruning
 MODEL_TYPE="up_down_lstm_prune"
-MODEL_ID="UpDownLSTM"
-SCHEDULER="cosine"
 BASELINE="/home/jiahuei/Documents/1_TF_files/relation_trans/mscoco_v1/UpDownLSTM__baseline"
+python /master/src/caption_vae/train_n_prune_transformer.py \
+    --caption_model ${MODEL_TYPE} \
+    --dataset_dir ${DATASET_DIR} \
+    --log_dir ${LOG_DIR} \
+    --lr_scheduler ${SCHEDULER} \
+    --learning_rate 0.01 \
+    --optim_epsilon 0.01 \
+    --drop_prob_lm 0.1 \
+    --batch_size 2 \
+    --batch_size_eval 2 \
+    --max_seq_length 10 \
+    --prune_type ${PRUNE_TYPE} \
+    --prune_sparsity_target ${PRUNE_SPARSITY_TARGET} \
+    --prune_supermask_sparsity_weight ${PRUNE_WEIGHT} \
+    --id TESTING_${MODEL_ID}__${PRUNE_TYPE}__${PRUNE_SPARSITY_TARGET} \
+    --save_checkpoint_every 10 \
+    --cache_min_free_ram ${CACHE_FREE_RAM}
 
-for PRUNE_TYPE in "supermask"; do
-    for PRUNE_SPARSITY_TARGET in 0.9875; do
-        for PRUNE_WEIGHT in 40; do
-            python /master/src/caption_vae/train_n_prune_transformer.py \
-                --caption_model ${MODEL_TYPE} \
-                --dataset_dir ${DATASET_DIR} \
-                --log_dir ${LOG_DIR} \
-                --lr_scheduler ${SCHEDULER} \
-                --learning_rate 0.01 \
-                --optim_epsilon 0.01 \
-                --drop_prob_lm 0.1 \
-                --prune_type ${PRUNE_TYPE} \
-                --prune_sparsity_target ${PRUNE_SPARSITY_TARGET} \
-                --prune_supermask_sparsity_weight ${PRUNE_WEIGHT} \
-                --id ZZZZZ_${MODEL_ID}__${PRUNE_TYPE}__${PRUNE_SPARSITY_TARGET} \
-                --save_checkpoint_every 10 \
-                --cache_min_free_ram ${CACHE_FREE_RAM}
-        done
-    done
-done
-
+exit 1
 # Fine-tune with mask frozen
-PRUNE_SPARSITY_TARGET=0.991
 BASELINE="${LOG_DIR}/${MODEL_ID}__supermask__${PRUNE_SPARSITY_TARGET}__wg_120.0/model_best_bin_mask.pth"
 EPOCHS=10
 SCST_NUM_SAMPLES=60
@@ -74,7 +120,8 @@ python /master/src/caption_vae/train_n_prune_transformer.py \
     --lr_scheduler step \
     --learning_rate 5e-5 \
     --learning_rate_decay_start -1 \
-    --batch_size 5 \
+    --batch_size 2 \
+    --batch_size_eval 2 \
     --max_epochs ${EPOCHS} \
     --drop_prob_lm 0.1 \
     --prune_type mask_freeze \
@@ -84,7 +131,7 @@ python /master/src/caption_vae/train_n_prune_transformer.py \
     --scst_sample ${SCST_SAMPLE} \
     --scst_baseline ${SCST_BASELINE} \
     --scst_bleu_weight 0,0,0,1 \
-    --id ZZZZZ_${MODEL_ID}__supermask__${PRUNE_SPARSITY_TARGET}__SCST_${SCST_SAMPLE}_${SCST_BASELINE}_s${SCST_NUM_SAMPLES}_e${EPOCHS}_C1B0001 \
+    --id TESTING_${MODEL_ID}__supermask__${PRUNE_SPARSITY_TARGET}__SCST_${SCST_SAMPLE}_${SCST_BASELINE}_s${SCST_NUM_SAMPLES}_e${EPOCHS}_C1B0001 \
     --save_checkpoint_every 10 \
     --cache_min_free_ram ${CACHE_FREE_RAM}
 
@@ -104,7 +151,7 @@ python /master/src/caption_vae/train_transformer.py \
     --dataset_dir ${DATASET_DIR} \
     --log_dir ${LOG_DIR} \
     --lr_scheduler ${SCHEDULER} \
-    --id ZZZZZ_${MODEL_ID}__baseline \
+    --id TESTING_${MODEL_ID}__baseline \
     --save_checkpoint_every 10 \
     --cache_min_free_ram ${CACHE_FREE_RAM}
 
@@ -131,32 +178,24 @@ python /master/src/caption_vae/train_transformer.py \
     --scst_sample ${SCST_SAMPLE} \
     --scst_baseline ${SCST_BASELINE} \
     --scst_bleu_weight 0,0,0,1 \
-    --id ZZZZZ_${MODEL_ID}__baseline__SCST_${SCST_SAMPLE}_${SCST_BASELINE}_s${SCST_NUM_SAMPLES}_e${EPOCHS}_C1B0001 \
+    --id TESTING_${MODEL_ID}__baseline__SCST_${SCST_SAMPLE}_${SCST_BASELINE}_s${SCST_NUM_SAMPLES}_e${EPOCHS}_C1B0001 \
     --save_checkpoint_every 10 \
     --cache_min_free_ram ${CACHE_FREE_RAM}
 
 
 # Pruning
 MODEL_TYPE="relation_transformer_prune"
-MODEL_ID="RTrans"
-SCHEDULER="noam"
-BASELINE="/home/jiahuei/Documents/1_TF_files/relation_trans/mscoco_v1/RTrans__baseline"
 
-for PRUNE_TYPE in "supermask"; do
-    for PRUNE_SPARSITY_TARGET in 0.9875; do
-        for PRUNE_WEIGHT in 40; do
-            python /master/src/caption_vae/train_n_prune_transformer.py \
-                --caption_model ${MODEL_TYPE} \
-                --dataset_dir ${DATASET_DIR} \
-                --log_dir ${LOG_DIR} \
-                --lr_scheduler ${SCHEDULER} \
-                --drop_prob_src 0.1 \
-                --prune_type ${PRUNE_TYPE} \
-                --prune_sparsity_target ${PRUNE_SPARSITY_TARGET} \
-                --prune_supermask_sparsity_weight ${PRUNE_WEIGHT} \
-                --id ZZZZZ_${MODEL_ID}__${PRUNE_TYPE}__${PRUNE_SPARSITY_TARGET} \
-                --save_checkpoint_every 10 \
-                --cache_min_free_ram ${CACHE_FREE_RAM}
-        done
-    done
-done
+python /master/src/caption_vae/train_n_prune_transformer.py \
+    --caption_model ${MODEL_TYPE} \
+    --dataset_dir ${DATASET_DIR} \
+    --log_dir ${LOG_DIR} \
+    --lr_scheduler ${SCHEDULER} \
+    --drop_prob_src 0.1 \
+    --prune_type ${PRUNE_TYPE} \
+    --prune_sparsity_target ${PRUNE_SPARSITY_TARGET} \
+    --prune_supermask_sparsity_weight ${PRUNE_WEIGHT} \
+    --id TESTING_${MODEL_ID}__${PRUNE_TYPE}__${PRUNE_SPARSITY_TARGET} \
+    --save_checkpoint_every 10 \
+    --cache_min_free_ram ${CACHE_FREE_RAM}
+
