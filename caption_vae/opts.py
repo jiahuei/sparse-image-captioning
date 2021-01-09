fe213730e29c61bfb37e459d91c28f7ee218c935
jiahuei <tanjiahuei@gmail.com>
2021-01-10 00:57:55 +0800

    allow `parse_opt()` to take argument list

--- caption_vae/opts.py
+++ caption_vae/opts.py
@@ -4,20 +4,22 @@ Created on 16 Sep 2020 17:21:25
 @author: jiahuei
 """
 import os
-from os.path import join as pjoin
 import logging
 from argparse import ArgumentParser, Namespace, ArgumentDefaultsHelpFormatter
 from utils.lightning import LightningModule
 from data import get_dataset, DATASET_REGISTRY
 from tokenizer import get_tokenizer, TOKENIZER_REGISTRY
 from models import get_model, MODEL_REGISTRY
-from models.relation_transformer import RelationTransformerModel
-from pruning.prune import PruningMixin
 
 logger = logging.getLogger(__name__)
 
 
-def parse_opt() -> Namespace:
+def parse_opt(arguments=None) -> Namespace:
+    if arguments is not None:
+        assert isinstance(arguments, list), (
+            f"Expected `arguments` to be a list, received: {type(arguments)}"
+        )
+
     # fmt: off
     # noinspection PyTypeChecker
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
@@ -53,7 +55,8 @@ def parse_opt() -> Namespace:
     #     default=None,
     #     help="str: Cache directory for `torchvision`."
     # )
-    args, unknown = parser.parse_known_args()
+    # fmt: on
+    args, unknown = parser.parse_known_args(arguments)
     LightningModule.add_argparse_args(
         parser.add_argument_group(
             "Training",
@@ -78,14 +81,7 @@ def parse_opt() -> Namespace:
             "Arguments for model, hyperparameters."
         )
     )
-    # PruningMixin.add_argparse_args(
-    #     parser.add_argument_group(
-    #         "Pruning",
-    #         "Arguments for weight pruning."
-    #     )
-    # )
-
-    args = parser.parse_args()
+    args = parser.parse_args(arguments)
 
     # Paths
     args.log_dir = os.path.join(args.log_dir, f"{args.id}")
