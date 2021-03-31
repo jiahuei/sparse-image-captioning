52ee89c0019520b6bcaca33a585a182abe1b90a4
jiahuei <tanjiahuei@gmail.com>
2021-03-31 18:41:09 +0800

    cleanup docker files

--- docker/docker_build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cd ${SCRIPT_ROOT}
-docker build -t jiahuei/pytorch:1.6.0-java8 -f ${SCRIPT_ROOT}/Dockerfile .
