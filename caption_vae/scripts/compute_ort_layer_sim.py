b72a69aee59ff086a75e863ee7d5f9fa0512121b
Tan Jia Huei <tanjiahuei@gmail.com>
2021-08-13 17:42:52 +0800

    `compute_ort_layer_sim.py`: add L2 normalisation

--- caption_vae/scripts/compute_ort_layer_sim.py
+++ caption_vae/scripts/compute_ort_layer_sim.py
@@ -28,53 +28,48 @@ def main(
         context="paper", fig_scale=1.5,
 ):
     sns.set_context(context, font_scale=3.0)
-    # palette = sns.color_palette("Blues_r", as_cmap=True)
+    # palette = sns.color_palette("flare", as_cmap=True)
     palette = sns.diverging_palette(220, 20, as_cmap=True)
     common_kwargs = dict(
         cmap=palette, annot=annot, annot_kws={"fontsize": 18}, fmt=".1f",
-        cbar=True, xticklabels=True, yticklabels=True
+        cbar=True, xticklabels=True, yticklabels=True, square=True,
     )
 
-    if not os.path.isfile(f"{os.path.join(output_dir, 'encoder')}.npy"):
-        state_dict = torch.load(
-            "/home/jiahuei/Documents/1_TF_files/relation_trans/mscoco_v1/RTrans__baseline/model_best.pth")
+    state_dict = torch.load(
+        "/home/jiahuei/Documents/1_TF_files/relation_trans/mscoco_v1/RTrans__baseline/model_best.pth")
 
-        encoder_params = [[] for _ in range(6)]
-        decoder_params = [[] for _ in range(6)]
-        encoder_layer_sizes = [0] * 6
-        decoder_layer_sizes = [0] * 6
-        for k, v in state_dict.items():
-            v = v.cpu().numpy().reshape((-1))
-            if k.startswith("model.encoder.layers"):
-                i = int(k.split(".")[3])
-                encoder_layer_sizes[i] += v.size
-                encoder_params[i].append(v)
-            elif k.startswith("model.decoder.layers"):
-                i = int(k.split(".")[3])
-                decoder_layer_sizes[i] += v.size
-                decoder_params[i].append(v)
-        print(encoder_layer_sizes)
-        print(decoder_layer_sizes)
+    encoder_params = [[] for _ in range(6)]
+    decoder_params = [[] for _ in range(6)]
+    encoder_layer_sizes = [0] * 6
+    decoder_layer_sizes = [0] * 6
+    for k, v in state_dict.items():
+        v = torch.nn.functional.normalize(v, p=2.0, dim=-1, eps=1e-12, out=None)
+        v = v.cpu().numpy().reshape((-1))
+        if k.startswith("model.encoder.layers"):
+            i = int(k.split(".")[3])
+            encoder_layer_sizes[i] += v.size
+            encoder_params[i].append(v)
+        elif k.startswith("model.decoder.layers"):
+            i = int(k.split(".")[3])
+            decoder_layer_sizes[i] += v.size
+            decoder_params[i].append(v)
+    print(encoder_layer_sizes)
+    print(decoder_layer_sizes)
 
-        encoder_params = np.array([np.concatenate(_) for _ in encoder_params])
-        decoder_params = np.array([np.concatenate(_) for _ in decoder_params])
-        print(encoder_params.shape)
-        print(decoder_params.shape)
+    encoder_params = np.array([np.concatenate(_) for _ in encoder_params])
+    decoder_params = np.array([np.concatenate(_) for _ in decoder_params])
+    print(encoder_params.shape)
+    print(decoder_params.shape)
 
-        encoder = compute_sim(encoder_params)
-        decoder = compute_sim(decoder_params)
-        np.save(f"{os.path.join(output_dir, 'encoder')}.npy", encoder)
-        np.save(f"{os.path.join(output_dir, 'decoder')}.npy", decoder)
-    else:
-        encoder = np.load(f"{os.path.join(output_dir, 'encoder')}.npy")
-        decoder = np.load(f"{os.path.join(output_dir, 'decoder')}.npy")
+    encoder = compute_sim(encoder_params)
+    decoder = compute_sim(decoder_params)
     matrices = dict(
         encoder=encoder,
         decoder=decoder,
     )
 
     for name, mat in tqdm(matrices.items()):
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3.75 * fig_scale, 3. * fig_scale))
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(4.5 * fig_scale, 3 * fig_scale))
         ax = sns.heatmap(mat, vmin=np.unique(mat)[1], ax=ax, **common_kwargs)
         # https://stackoverflow.com/a/51760629
         ax.xaxis.tick_top()
