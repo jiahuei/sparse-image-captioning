c864fb0b5f61c46c5d71f5933cc772d15f8d5b70
Tan Jia Huei <tanjiahuei@gmail.com>
2021-11-19 16:01:48 +0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,9 +54,13 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+# html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
+# html_theme_path = [
+#     "_themes",
+# ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
--- docs/index.rst
+++ docs/index.rst
@@ -6,11 +6,13 @@
 Welcome to SparCa's documentation!
 ==================================
 
+
 .. toctree::
    :maxdepth: 2
    :caption: Get Started
 
    get_started.md
+
 
 
 Indices and tables
